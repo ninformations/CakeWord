67fc34482ae0fff5e59606e283c9c83ffcbe49bc
Nitin Sharma <ninformations@users.noreply.github.com>
2016-05-01 11:51:54 +0530

    updated bootstrap.

--- config/bootstrap.php
+++ config/bootstrap.php
@@ -1,12 +1,13 @@
 <?php
 use Cake\Event\EventManager;
+
 EventManager::instance()
     ->on(
         'Controller.initialize',
         function (Cake\Event\Event $event) {
             $controller = $event->subject();
             if ($controller->components()->has('RequestHandler')) {
-                $controller->RequestHandler->config('viewClassMap.word', 'CakeWord.Word');
+                $controller->RequestHandler->config('viewClassMap', ['word' => 'CakeWord.Word']);
             }
         }
     );
